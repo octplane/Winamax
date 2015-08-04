dfa2c92567e8d926a52f0127db1983b94e079e72
Pierre Baillet <oct@uranium.zoy.org>
2015-08-05 00:17:37 +0200

    License

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject app "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 		[twitter-api "0.7.8"]]
   :main app.core
